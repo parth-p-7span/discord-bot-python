d7c11270f3e49dfbae75cede41c4a7b36472d7b0
Parth Panchal <parth.p@7span.com>
2022-09-10 15:40:21 +0530

    constants time changed to utc

--- constants.py
+++ constants.py
@@ -23,13 +23,13 @@ STATUS_OK = 200
 STATUS_NO_CONTENT = 404
 STATUS_BAD_REQUEST = 400
 
-# auto message timing in (hours, minutes) - 24 hours format
-MORNING_TIME = ("11", "0")
-EVENING_TIME = ("19", "0")
-REPORTING_TIME = ("11", "30")
-CELEBRATE_TIME = ("6", "0")
-MORNING_THREAD_TIME = ("9", "45")
-EVENING_THREAD_TIME = ("19", "0")
+# auto message timing in (hours, minutes) - 24 hours format UTC Timezone
+MORNING_TIME = ("5", "30")
+EVENING_TIME = ("13", "30")
+REPORTING_TIME = ("6", "0")
+CELEBRATE_TIME = ("0", "30")
+MORNING_THREAD_TIME = ("4", "15")
+EVENING_THREAD_TIME = ("13", "30")
 
 
 # discord channel ids
